fee830a015bea60dd5af4f095b1d15453209c2e8
Ramón París <pariis78@gmail.com>
2020-01-16 23:34:34 -0300

    Se aplica middleware para formatear body de las requests y se juega con headers

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 router.get("/hello-world", function(req, res) {
-  res.send("Hola mundo desde get");
+  console.log(req.headers);
+  res.header({
+    "custom-header": "This is a custom header"
+  });
+  res.send({ text: "Hola mundo!" });
 });
 
 router.post("/hello-world", function(req, res) {
+  console.log(req.query);
+  console.log(req.body);
   res.send("Hola mundo desde post");
 });
 
@@ -24,5 +32,6 @@ router.delete("/hello-world", function(req, res) {
   res.send("Hola mundo desde delete");
 });
 
-app.listen(3000);
-console.log("The app is running at port http://localhost:3000");
+app.listen(3000, () => {
+  console.log("The app is running at port http://localhost:3000 🚀");
+});
