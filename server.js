204d1afad9c40c1ed0408a6fbe33670da0df7f3d
Ramón París <pariis78@gmail.com>
2020-01-16 21:32:09 -0300

    - Hello world con express y verbos http - eslint

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
-app.use('/', (req, res)=>{
-    res.send("Hola!")
+app.use(router);
+
+router.get("/hello-world", function(req, res) {
+  res.send("Hola mundo desde get");
+});
+
+router.post("/hello-world", function(req, res) {
+  res.send("Hola mundo desde post");
+});
+
+router.put("/hello-world", function(req, res) {
+  res.send("Hola mundo desde put");
+});
+
+router.patch("/hello-world", function(req, res) {
+  res.send("Hola mundo desde patch");
+});
+
+router.delete("/hello-world", function(req, res) {
+  res.send("Hola mundo desde delete");
 });
 
 app.listen(3000);
